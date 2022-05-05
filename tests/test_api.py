10e9c26d856bde709a98e615688712a6019bde04
hi9105 <dialogssoftware@googlemail.com>
2022-05-05 02:25:19 +0200

    add sys path

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../Deploy-ML-Model-Heroku-FastAPI')
+sys.path.append('/home/hihi1/Deploy-ML-Model-Heroku-FastAPI')
 
 from fastapi.testclient import TestClient
 from main import app
