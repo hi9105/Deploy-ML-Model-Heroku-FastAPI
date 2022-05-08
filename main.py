ee8fefb3fbbb7ee65f275a880cb2ed41648d76fa
hi9105 <dialogssoftware@googlemail.com>
2022-05-08 13:50:42 +0200

    update data example

--- main.py
+++ main.py
@@ -61,16 +61,16 @@ class CensusData(BaseModel):
                 "workclass": "Private",
                 "fnlgt": 201490,
                 "education": "HS-grad",
-                "education_num": 9,
-                "marital_status": "Never-married",
+                "education-num": 9,
+                "marital-status": "Never-married",
                 "occupation": "Adm-clerical",
                 "relationship": "Own-child",
                 "race": "White",
                 "sex": "Male",
-                "capital_gain": 0,
-                "capital_loss": 0,
-                "hours_per_week": 20,
-                "native_country": "United-States",
+                "capital-gain": 0,
+                "capital-loss": 0,
+                "hours-per-week": 20,
+                "native-country": "United-States",
             }
         }
         alias_generator = underscore_to_hyphen
